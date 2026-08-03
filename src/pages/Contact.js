7e91b2a3edd094c23b2d53ee71f290f5fa36d7f8
Mythily Rajeshwari <karthikeyan@gmail.com>
2026-08-03 11:25:55 +0530

    Initial commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/esm/Container';
 import {
     FaTwitter,
     FaFacebookF,
-    FaYoutube,
+    FaEnvelope,
     FaInstagram,
     FaWhatsapp,
 } from "react-icons/fa";
@@ -29,34 +29,47 @@ export default function Contact() {
 
                     <div className='col-md-3'>
                         <h2 className='textcolor'>Contact</h2>
-                        <p>📍 41/1 HUDB Colany Peelamedu Coimbatore</p>
+                        <p>📍 41/1 HUDCO Colony Peelamedu Coimbatore</p>
                         <p>📞 9092330880</p>
-                        <p>📫 srineevi@gmail.com</p>
+                        <p>📫 srineevicoffeehouse@gmail.com</p>
                         <div className="Social">
                             <div className="iconborder">
                                 <Link
                                     className="iconcolor"
-                                    to="mailto:akarthikeyan.0718@gmail.com"
+                                    to="mailto:srineevicoffeehouse.25@gmail.com"
+                                    target="_blank"
+                                    aria-label="Visit our Facebook page"
+                                >
+                                    <FaEnvelope className="icon-color" />
+                                </Link>
+                            </div>
+
+                            <div className="iconborder1">
+                                <Link
+                                    className="iconcolor"
+                                    to="https://www.facebook.com/"
                                     target="_blank"
                                     aria-label="Visit our Facebook page"
                                 >
                                     <FaFacebookF className="icon-color" />
                                 </Link>
                             </div>
+
                             <div className="iconborder2">
                                 <Link
                                     className="iconcolor"
-                                    to="https://wa.me/9524709513"
+                                    to="https://wa.me/9092330880"
                                     target="_blank"
                                     aria-label="Visit our Whatsapp page"
                                 >
                                     <FaWhatsapp className="icon-color" />
                                 </Link>
                             </div>
+                            
                             <div className="iconborder3">
                                 <Link
                                     className="iconcolor"
-                                    to="https://www.instagram.com/cute-boy-sriram-007"
+                                    to="https://www.instagram.com/srineevicoffeehouse2026/"
                                     target="_blank"
                                     aria-label="Visit our Instagram page"
                                 >
@@ -71,9 +84,9 @@ export default function Contact() {
                     <div className='col-md-3'>
                         <h2 className='textcolor'>Opening</h2>
                         <h3>Monday - Saturday</h3>
-                        <p>07AM - 08PM</p>
+                        <p>7:00AM - 8:00PM</p>
                         <h3>Sunday</h3>
-                        <p>08AM - 07PM</p>
+                        <p>8:00AM - 7:00PM</p>
                     </div>
 
                     <div className='col-md-3'>
@@ -89,9 +102,9 @@ export default function Contact() {
 
                 </div>
             </Container>
-            <hr/>
+            <hr />
             <div className='Copy'>
-                Copyright @ 2026 All Right Reserver By SRI NEEVI
+                Copyright @ 2026 All Right Reserver By SRI NEEVI COFFEE HOUSE <span className='textcolor'>|</span> Designed By <span><a href="https://karthi-frontend-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer"  className='textcolor'>Karthi-007</a></span>
             </div>
         </div>
     )
